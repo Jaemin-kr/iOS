7b3a5b52b4fbf341ef1928a81e003fa9eea38019
Jaemin-Mac <jaemin.ko98@gmail.com>
2022-12-02 17:17:56 +0900

    first swift study

---
+++ swift/MyPlayground.playground/Contents.swift
@@ -0,0 +1,40 @@
+import UIKit
+
+var greeting = "Hello, playground"
+
+let age: Int = 10
+
+"Hello My age is \(age)"
+"Hello My age is \(age+15)"
+
+print("Hello My age is \(age+15)")
+
+class Person{
+    var name: String = "Jaemin"
+    var age: Int = 10
+}
+
+let jaemin: Person = Person()
+
+print(jaemin)
+
+print("\n------------\n")
+
+dump(jaemin)
+
+//상수와 변수
+let num : Int = 10
+var num_var : Int = 5
+let a : Int = 4
+var sum : Int
+
+sum = a+num_var
+
+print("\(num)")
+print("\(num_var)")
+
+num_var = 25
+
+print("\(num_var)")
+print("\(sum)")
+
