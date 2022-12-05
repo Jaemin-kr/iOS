f02da81d3d52ad7ee4bde08a70d1e09db6096b1e
Jaemin-Mac <jaemin.ko98@gmail.com>
2022-12-05 13:48:19 +0900

    Swift Data type

--- swift/MyPlayground.playground/Contents.swift
+++ swift/MyPlayground.playground/Contents.swift
@@ -2,12 +2,12 @@ import UIKit
 
 var greeting = "Hello, playground"
 
-let age: Int = 10
+//let age: Int = 10
 
-"Hello My age is \(age)"
-"Hello My age is \(age+15)"
-
-print("Hello My age is \(age+15)")
+//"Hello My age is \(age)"
+//"Hello My age is \(age+15)"
+//
+//print("Hello My age is \(age+15)")
 
 class Person{
     var name: String = "Jaemin"
@@ -71,3 +71,61 @@ var capital_city: [String: String] = [:]
 //다른표현, 생성자 사용
 var language_arr1 = [String]()
 var cities = [String:String]()
+
+//221205
+//Object oriented programming
+class SomeClass{
+    var someProperty: Any = 1
+    func someMethod() {
+        //some task
+    }
+}
+
+let myInstance: SomeClass = SomeClass()
+//constant myInstance에 someClass할당
+//생성자를 통해 메모리에 할당되고 초기화한 객체를 인스턴스라고 부름
+
+myInstance.someProperty = 100
+print(myInstance.someProperty)
+
+myInstance.someMethod()
+
+
+//functional programming
+func doSomething(){
+    print("do something")
+}
+
+func doAnotherThing(){
+    print("do another thing")
+}
+
+func execute(tasks: [()->Void]){
+    for task in tasks{
+        task()
+    }
+}
+
+execute(tasks: [doSomething, doAnotherThing])
+
+func sum(first:Int) -> ((Int)->Int){
+    return { second in first + second }
+}
+sum(first: 10)(5)
+
+var j: Int = 0
+
+for i in 0...3{
+    j += i
+}
+
+
+var name: String = "jaemin"
+var age: Int = 25
+var job = "Student"
+var height = 178.8
+
+age = 99
+job = "iOS Programmer"
+
+print("저의 이름은 \(name)이고, 나이는 \(age)세며, 직업은 \(job)입니다. 비밀이지만, 키는 \(height)cm 입니다.")
